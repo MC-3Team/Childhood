dfa26da9ad699848422d1eebf0051e7c5249591c
Waffle <enrico.maricar@gmail.com>
2024-08-05 01:14:36 +0700

    Initial Commit

---
+++ BambiniMath/BambiniMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  BambiniMathApp.swift
+//  BambiniMath
+//
+//  Created by Enrico Maricar on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BambiniMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
